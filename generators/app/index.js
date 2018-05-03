3576683d47d634af62620dfa4374726f7e5e739f
TT <codertina@gmail.com>
2018-05-03 20:58:55 +0530

    Tina | Added user prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,20 +1,14 @@
 var Generator = require('yeoman-generator');
+var prompts = require('./prompts');
 
 module.exports = class extends Generator {
-  // The name `constructor` is important here
   constructor(args, opts) {
-    // Calling the super constructor is important so our generator is correctly set up
     super(args, opts);
-
-    // Next, add your custom code
-    this.option('babel'); // This method adds support for a `--babel` flag
   }
-
+   prompting() {
+      return this.prompt(prompts).then(answers => this.answers = answers);
+  }
    method1() {
-    this.log('method 1 just ran');
-  }
-
-  method2() {
-    this.log('method 2 just ran');
+    this.log('Your settins are ' + JSON.stringify(this.answers));
   }
 };
---
+++ generators/app/prompts.js
@@ -0,0 +1,9 @@
+const prompts = [{
+    type    : 'input',
+    name    : 'name',
+    message : 'Your project name',
+    default : this.appname
+  }];
+  
+  module.exports = prompts;
+  

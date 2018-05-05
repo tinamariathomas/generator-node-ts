b2a1f14a2582dbb16b02d12266d156ead856ebe0
TT <codertina@gmail.com>
2018-05-05 14:36:38 +0530

    Tina | Jest configuration for test

--- generators/app/templates/service/src/app.ts
+++ generators/app/templates/service/src/app.ts
@@ -1,7 +1,8 @@
 import * as express from "express";
+import healthCheckHandler from "./handlers/health";
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", healthCheckHandler);
 
 // tslint:disable-next-line:no-console
 app.listen(3000, () => console.log("Example app listening on port 3000!"));

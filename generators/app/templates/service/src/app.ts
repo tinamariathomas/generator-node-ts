9482f0d389ee55d1c71c7998228beffabdd7ed7d
TT <codertina@gmail.com>
2018-05-05 11:34:56 +0530

    Tina | Added linting

--- generators/app/templates/service/src/app.ts
+++ generators/app/templates/service/src/app.ts
@@ -1,6 +1,7 @@
-import * as express from 'express';
-const app = express()
+import * as express from "express";
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// tslint:disable-next-line:no-console
+app.listen(3000, () => console.log("Example app listening on port 3000!"));

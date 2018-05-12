19c1137479e0bbb203c7c63835c1e2d3f0bee6bf
TT <codertina@gmail.com>
2018-05-12 16:45:20 +0530

    Tina | Add method routes get and post to routes interface

--- generators/app/templates/service/src/app.ts
+++ generators/app/templates/service/src/app.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import routes from "./routes";
 const app = express();
 
-routes.forEach((route) => app.get(route.path, route.handler));
+routes.forEach((route) => app[route.method](route.path, route.handler));
 
 // tslint:disable-next-line:no-console
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
--- generators/app/templates/service/src/routes.ts
+++ generators/app/templates/service/src/routes.ts
@@ -1,13 +1,19 @@
 import {RequestHandler} from "express-serve-static-core";
 import healthCheckHandler from "./handlers/health";
 
+enum Method{
+    get = 'get',
+    post = 'post',
+}
+
 export interface IRoute {
     path: string;
     handler: RequestHandler;
+    method: Method;
 }
 
 const routes : IRoute[] = [
-    {path: '/', handler: healthCheckHandler},
+    {path: '/', handler: healthCheckHandler, method: Method.get},
 ];
 
 export default routes;
